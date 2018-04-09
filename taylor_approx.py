ceddb6eced1806548c74560f78a965a190ca4ba1
Daniel <Chang254@mail.chapman.edu>
2018-04-09 19:42:42 +0000

    completed function

--- taylor_approx.py
+++ taylor_approx.py
@@ -14,15 +14,15 @@ import array_calculus as ac
 def taylor(x,f,i,n):
     a = x[i]
     d = ac.derivative(x[i],x[i]+100,101)
+    d2 = ac.second_derivative(x[i],x[i]+100,101)
     dfdx = np.matmul(d,f[i])
-    
+    d2fdx2 = np.matmul(d2,f[i])
     if n == 0:
         fapprox = f[i]
-        
     elif n == 1:
         fapprox = f[i] + dfdx[0]*(x-a)
     elif n == 2:
-        fapprox = f[i] + dfdx[0]*(x-a)
+        fapprox = f[i] + dfdx[0]*(x-a) + d2fdx2[0]*((x-a)**2)/2
     else:
         return print("Invalid n, please choose a new n")
     return (x,fapprox)
