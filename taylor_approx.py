20fbfc44cc88e193465a7a72a1db5e0688e75f55
Devon <dball@chapman.edu>
2018-04-12 21:46:46 +0000

    fixed dimensional error by replacing * with @ in line 45

--- taylor_approx.py
+++ taylor_approx.py
@@ -35,14 +35,14 @@ def taylor(x,f,i,n):
     a = x[i]
     N = np.size(x)
     fa = f[i]*np.ones_like(x)
-    d = ac.derivative(x[0],x[N-1],N)
+    D = ac.derivative(x[0],x[N-1],N)
     fact = 1
     fapprox = fa
-    dk = np.eye(N)
+    Dk = np.eye(N)
     for k in range(1,n+1):
         fact = fact*(k)
-        dk = np.matmul(dk,d)
-        fapprox += (dk*(x-a)**k)/fact
+        Dk = np.matmul(Dk,D)
+        fapprox += (Dk@((x-a)**k))/fact
     return (x,fapprox)
 
 def tanh(x):
